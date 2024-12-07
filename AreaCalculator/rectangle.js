2d62520553aca7e2430aad88a0b82e631fc00eba
Linkan Roy <90970218+linkan-roy@users.noreply.github.com>
2024-12-07 14:43:31 +0600

    submit js assignment

---
+++ AreaCalculator/rectangle.js
@@ -0,0 +1,4 @@
+const lenght = Number(prompt("Enter the lenght of a rectangle: "));
+const width = Number(prompt("Enter the width of a rectangle: "));
+
+console.log(`Area of rectangle ${lenght * width}`);
---
+++ AreaCalculator/triangle.js
@@ -0,0 +1,4 @@
+const base = Number(prompt("Enter the base of a triangle: "));
+const height = Number(prompt("Enter the height of a triangle: "));
+
+console.log(`Area of triangle ${0.5 * base * height}`);
